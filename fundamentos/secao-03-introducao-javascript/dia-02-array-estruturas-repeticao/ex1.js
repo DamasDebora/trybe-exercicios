dbb52f763f61793858e448bacf86df76187ffe29
Debora Damas <damasdebora32@gmail.com>
2023-05-29 12:29:40 -0300

    ex 3

--- fundamentos/secao-03-introducao-javascript/dia-02-array-estruturas-repeticao/ex1.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-array-estruturas-repeticao/ex1.js
@@ -8,15 +8,19 @@ for (let i =0 ; i < numbers.length; i++){
 
 //Some todos os valores contidos no array e imprima o resultado.
 let sum = 0 
-
 for (let i =0 ; i < numbers.length; i++){
     sum += numbers[i]
 }
 console.log(`soma: ${sum}`)
 
-/*Calcule e imprima a média aritmética dos valores contidos no array. A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+//Calcule e imprima a média aritmética dos valores contidos no array. A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+for (let i =0 ; i < numbers.length; i++){
+    sum += numbers[i]
+}
+let media = sum / numbers.length
+console.log(`media: ${media}`)
 
-Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+/*Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
 
 Utilizando for, descubra o maior valor contido no array e imprima-o.
 
