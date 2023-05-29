7bd66e5681e3ded239bce985fb21117fe835fb50
Debora Damas <damasdebora32@gmail.com>
2023-05-29 12:51:05 -0300

    ex 4

--- fundamentos/secao-03-introducao-javascript/dia-02-array-estruturas-repeticao/ex1.js
+++ fundamentos/secao-03-introducao-javascript/dia-02-array-estruturas-repeticao/ex1.js
@@ -20,9 +20,14 @@ for (let i =0 ; i < numbers.length; i++){
 let media = sum / numbers.length
 console.log(`media: ${media}`)
 
-/*Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+//Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+if (media > 20){
+    console.log('O valor da média aritméticaé maior que 20')
+} else {
+    console.log('O valor da média aritmética é menor ou igual a 20')
+}
 
-Utilizando for, descubra o maior valor contido no array e imprima-o.
+/*Utilizando for, descubra o maior valor contido no array e imprima-o.
 
 Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
 */
