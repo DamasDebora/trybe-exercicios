2d9d51b5b9471e3c6ee5e32b511e8b813e98e164
Debora Damas <damasdebora32@gmail.com>
2023-06-02 12:33:02 -0300

    Atividade 2

--- fundamentos/secao-03-introducao-javascript/dia-04-objects/manipulacao3.js
+++ fundamentos/secao-03-introducao-javascript/dia-04-objects/manipulacao3.js
@@ -11,26 +11,35 @@ const school = {
         professor: 'Carlos Patrício',
         shift: 'Manhã',
       },
+      {
+        course: 'Kotlin',
+        students: 10,
+        professor: 'Gabriel Oliva',
+        shift: 'Noite',
+      },
+      {
+        course: 'JavaScript',
+        students: 738,
+        professor: 'Gustavo Caetano',
+        shift: 'Tarde',
+      },
     ], 
-    lessons2: [
-          {
-            course: 'Kotlin',
-            students: 10,
-            professor: 'Gabriel Oliva',
-            shift: 'Noite',
-          },
-          {
-            course: 'JavaScript',
-            students: 738,
-            professor: 'Gustavo Caetano',
-            shift: 'Tarde',
-          },
-    ]
 
 };
 
 //Crie uma função que obtenha o valor da chave de acordo com sua posição no array.
 
 const getValue = (obj, index) => Object.values(obj)[index];
+console.log(getValue(school, 0));
 
-console.log(getValue(school, 1));
+//Crie uma função que retorne a soma do número total de estudantes em todos os cursos.
+const quantidadeEstudantes = (obj) => {
+
+    let count = 0 
+    for(let i = 0; i < obj.lessons.length; i += 1){
+        count += obj.lessons[i].students
+    }
+    return count
+}
+
+console.log(quantidadeEstudantes(school))
