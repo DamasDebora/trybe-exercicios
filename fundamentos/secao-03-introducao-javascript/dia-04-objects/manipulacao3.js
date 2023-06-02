6c4446ab31472297d073cc2c41f221b4687e79ec
Debora Damas <damasdebora32@gmail.com>
2023-06-02 12:40:28 -0300

    Rename function

--- fundamentos/secao-03-introducao-javascript/dia-04-objects/manipulacao3.js
+++ fundamentos/secao-03-introducao-javascript/dia-04-objects/manipulacao3.js
@@ -33,7 +33,7 @@ const getValue = (obj, index) => Object.values(obj)[index];
 console.log(getValue(school, 0));
 
 //Crie uma função que retorne a soma do número total de estudantes em todos os cursos.
-const quantidadeEstudantes = (obj) => {
+const getStudents = (obj) => {
 
     let count = 0 
     for(let i = 0; i < obj.lessons.length; i += 1){
@@ -41,5 +41,6 @@ const quantidadeEstudantes = (obj) => {
     }
     return count
 }
+console.log(getStudents(school))
 
-console.log(quantidadeEstudantes(school))
+
