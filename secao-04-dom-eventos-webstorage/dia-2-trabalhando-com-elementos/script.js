158be256ee319afe5cd17a465681957d9101086f
Debora Damas <damasdebora32@gmail.com>
2023-06-11 16:56:55 -0300

    ex 2

--- secao-04-dom-eventos-webstorage/dia-2-trabalhando-com-elementos/script.js
+++ secao-04-dom-eventos-webstorage/dia-2-trabalhando-com-elementos/script.js
@@ -3,3 +3,8 @@ const body = document.body
 const h1 = document.createElement('h1')
 h1.innerText = 'TrybeTrip - Agência de Viagens'
 body.appendChild(h1)
+
+const main = document.createElement('main')
+main.className = 'main-content'
+body.appendChild(main)
+
