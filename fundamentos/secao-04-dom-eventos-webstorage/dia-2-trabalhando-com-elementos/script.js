5ad583d8603e7e283a41b4e03e32b961a8c1dec3
Debora Damas <damasdebora32@gmail.com>
2023-06-12 09:03:50 -0300

    +

---
+++ fundamentos/secao-04-dom-eventos-webstorage/dia-2-trabalhando-com-elementos/script.js
@@ -0,0 +1,45 @@
+const body = document.body
+
+const h1 = document.createElement('h1')
+h1.innerText = 'TrybeTrip - Agência de Viagens'
+body.appendChild(h1)
+
+const main = document.createElement('main')
+main.className = 'main-content'
+body.appendChild(main)
+
+const section = document.createElement('section')
+section.className = 'center-content'
+main.appendChild(section)
+
+const p = document.createElement('p')
+section.appendChild(p)
+
+const sectionLeft = document.createElement('section')
+sectionLeft.className = 'left-content'
+main.appendChild(sectionLeft)
+
+const sectionRight = document.createElement('section')
+sectionRight.className = 'right-content'
+main.appendChild(sectionRight)
+
+const img = document.createElement('img')
+img.src = 'https://picsum.photos/200'
+img.className = 'small-image'
+sectionLeft.appendChild(img)
+
+const ul = document.createElement('ul')
+const numbers = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez']
+
+for(let i =0; i < numbers.length; i += 1){
+    const li = document.createElement('li')
+    li.innerText = numbers[i]
+    ul.appendChild(li)
+}
+sectionRight.appendChild(ul)
+
+for (let i = 0; i < 3; i += 1){
+    const h3 = document.createElement('h3')
+    h3.innerText = i
+    main.appendChild(h3)
+}
