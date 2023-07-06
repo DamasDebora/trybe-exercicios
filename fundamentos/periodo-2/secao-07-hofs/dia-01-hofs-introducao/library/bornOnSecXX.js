850790c0104ae42be4390ea1c1a828b903b2e603
Debora Damas <damasdebora32@gmail.com>
2023-07-06 16:38:10 -0300

    was everyone born on sec xx?

---
+++ fundamentos/periodo-2/secao-07-hofs/dia-01-hofs-introducao/library/bornOnSecXX.js
@@ -0,0 +1,13 @@
+const books = require('./booksData')
+
+//Faça uma função que retorne true se todas as pessoas autoras tiverem nascido no século XX, ou false, caso contrário.
+const expectedResult = false;
+
+function everyoneWasBornOnSecXX() {
+  // escreva seu código aqui
+	return books.every((book) => (
+		book.author.birthYear >= 1901 && book.author.birthYear <= 2000
+	))
+}
+
+everyoneWasBornOnSecXX()
