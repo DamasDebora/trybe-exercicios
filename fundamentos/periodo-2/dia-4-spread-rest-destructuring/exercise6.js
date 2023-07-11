337e80bcc962a974b68c8f0b56bcb018121d151f
Debora Damas <damasdebora32@gmail.com>
2023-07-11 14:43:44 -0300

    feat: ex6 - done

---
+++ fundamentos/periodo-2/dia-4-spread-rest-destructuring/exercise6.js
@@ -0,0 +1,13 @@
+// desestruture as estações do ano e espalhe-as em um array de meses do ano
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+// yearSeasons: estações do ano.
+
+const { spring, summer, autumn, winter } = yearSeasons
+const monts = [...spring, ...summer, ...autumn, ...winter]
+console.log(monts)
