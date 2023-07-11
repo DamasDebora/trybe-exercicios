e78280dac87acaf5d20ed321b9d4b7bd15121969
Debora Damas <damasdebora32@gmail.com>
2023-07-11 10:16:37 -0300

    feat: ex4 - structure

---
+++ fundamentos/periodo-2/dia-4-spread-rest-destructuring/exercise4.js
@@ -0,0 +1,32 @@
+// retorna todas as pessoas australianas que nasceram no século 20
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+  // bornIn: nascido em
+];
+
+// escreva filterPeople abaixo
